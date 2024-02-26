13d944069d178518ca49edbbb62189d4416fb5a3
angelashi <angelas2026@student.cis.edu.hk>
2024-02-26 14:14:37 +0800

    Finished tutorial 8.2

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList <String> listeners = new ArrayList();
+    public Song (String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany(ArrayList <String> newListeners){
+        int newListenerCount = 0;
+        for (int i = 0; i < newListeners.size(); i++){
+            String lowercaseListener = newListeners.get(i).toLowerCase();
+            if (!(listeners.contains(lowercaseListener))){
+                listeners.add(lowercaseListener);
+                newListenerCount++;
+            }
+        }
+        return newListenerCount;
+    }
 }
